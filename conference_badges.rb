00779f5b37a503b668153a8b663c06f983007aff
Dominic Chapman <dominicchapman@me.com>
2017-10-03 21:46:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,5 +8,5 @@ def badge_maker_creator(names)
 end
 
 def assign_rooms(names)
-  names.each_with_index{ |name, index| puts "Hello, #{name}! You'll be assigned to room #{index}" }
+  names.each_with_index{ |name, index| puts "Hello, #{name}! You'll be assigned to room #{index+1}" }
 end
