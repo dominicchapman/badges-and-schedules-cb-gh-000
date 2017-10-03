ddb0ae71213ab8ab03dc3e5eaf607fb14968d168
Dominic Chapman <dominicchapman@me.com>
2017-10-03 21:40:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,8 @@
 # Write your code here.
 def badge_marker(names)
   names.each{ |name| puts "Hello, my name is #{name}." }
+end
 
+def assign_rooms(names)
+  names.each_with_index{ |name, index| puts "Hello, #{name}! You'll be assigned to room #{index|}" }
 end
